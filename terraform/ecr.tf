9e99ed6bf078041dea9c78b6fd0bc51f9020846e
Anthony Viera <anthonyviera4@gmail.com>
2024-12-19 01:29:29 -0600

    ooops

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,3 @@
-resource "aws_ecr_repository" "desserts_api_lambdas" {
-  name                 = var.environment == "prod" ? "desserts-api-lambdas-us-east-1" : "dev-desserts-api-lambdas-us-east-1"
-  image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "desserts_api_lambdas" {
+  name = var.environment == "prod" ? "desserts-api-lambdas-us-east-1" : "dev-desserts-api-lambdas-us-east-1"
 }
