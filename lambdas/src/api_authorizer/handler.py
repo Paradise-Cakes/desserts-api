c8cd69b13d2c43fb720073fc5e237b7b005fdba1
Anthony Viera <36007311+Anthony9698@users.noreply.github.com>
2024-12-21 20:22:31 -0600

    Stop using api gw module? (#3)

---
+++ lambdas/src/api_authorizer/handler.py
@@ -0,0 +1,38 @@
+import jwt
+from aws_lambda_powertools import Logger
+
+logger = Logger()
+
+
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    try:
+        resource = event.get("methodArn")
+        token = event.get("authorizationToken").split("Bearer ")[1]
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+
+        user_email = decoded_token.get("email")
+        user_groups = decoded_token.get("cognito:groups", [])
+
+        return generate_policy("Allow", resource, user_email, user_groups)
+
+    except Exception as e:
+        logger.exception(e)
+        return generate_policy("Deny", event["methodArn"], None, None)
+
+
+def generate_policy(effect, resource, user_email, user_groups):
+    policy = {
+        "principalId": user_email if user_email else "Unknown",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {"Action": "execute-api:Invoke", "Effect": effect, "Resource": resource}
+            ],
+        },
+        "context": {
+            "user_email": user_email,
+            "user_groups": user_groups if user_groups else [],
+        },
+    }
+    return policy
--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,29 +1,220 @@
-module "api_gateway" {
-  source = "git@github.com:Paradise-Cakes/pc-terraform-modules.git//apiGateway?ref=v1.8.0"
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "desserts-api-gateway"
+  description = "Proxy to handle requests to desserts API"
 
-  app_arn                 = aws_lambda_function.app.arn
-  api_gateway_name        = "desserts-api-gateway"
-  api_description         = "Proxy to handle requests to desserts API"
-  binary_media_types      = ["multipart/form-data"]
-  stage_name              = "v1"
-  api_acm_certificate_arn = data.aws_acm_certificate.desserts_api.arn
-  api_domain_name         = data.aws_acm_certificate.desserts_api.domain
-  lambda_function_arn     = aws_lambda_function.app.invoke_arn
-  environment             = var.environment
-  api_zone_id             = data.aws_route53_zone.desserts_api.zone_id
-  website_zone_id         = data.aws_route53_zone.paradise_cakes.zone_id
+  binary_media_types = ["multipart/form-data"]
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+
+resource "aws_api_gateway_method" "get_desserts" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_desserts_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.get_desserts.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app.invoke_arn
+}
+
+resource "aws_api_gateway_method" "post_desserts" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "POST"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "post_desserts_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.post_desserts.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app.invoke_arn
+
+  request_parameters = {
+    "integration.request.header.user-email"  = "context.user_email"
+    "integration.request.header.user-groups" = "context.user_groups"
+  }
+}
+
+resource "aws_api_gateway_method" "patch_desserts" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "PATCH"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "patch_desserts_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.patch_desserts.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app.invoke_arn
+
+  request_parameters = {
+    "integration.request.header.user-email"  = "context.user_email"
+    "integration.request.header.user-groups" = "context.user_groups"
+  }
+}
+
+resource "aws_api_gateway_method" "delete_desserts" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "DELETE"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "delete_desserts_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.delete_desserts.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app.invoke_arn
+
+  request_parameters = {
+    "integration.request.header.user-email"  = "context.user_email"
+    "integration.request.header.user-groups" = "context.user_groups"
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name           = "v1"
+  rest_api_id          = aws_api_gateway_rest_api.rest_api.id
+  deployment_id        = aws_api_gateway_deployment.deployment.id
+  xray_tracing_enabled = true
+  cache_cluster_size   = "0.5"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  depends_on = [
+    aws_api_gateway_integration.get_desserts_integration,
+    aws_api_gateway_integration.post_desserts_integration,
+    aws_api_gateway_integration.patch_desserts_integration,
+    aws_api_gateway_integration.delete_desserts_integration,
+    aws_api_gateway_integration.cors,
+  ]
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_domain_name" "domain_name" {
+  certificate_arn = data.aws_acm_certificate.desserts_api.arn
+  domain_name     = data.aws_acm_certificate.desserts_api.domain
+}
+
+resource "aws_api_gateway_base_path_mapping" "base_path_mapping" {
+  api_id      = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  domain_name = aws_api_gateway_domain_name.domain_name.domain_name
+  base_path   = aws_api_gateway_stage.stage.stage_name
+}
+
+resource "aws_api_gateway_rest_api_policy" "rest_api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
 
   policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "execute-api:Invoke",
-      "Resource": ["execute-api:/*/*/*"]
-    }
-  ]
-}
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "execute-api:Invoke",
+        "Resource": ["execute-api:/*/*/*"]
+      }
+    ]
+  }
 EOF
+}
+
+resource "aws_api_gateway_method" "cors" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "statusCode": 200
+    }
+    EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  status_code = 200
+
+  depends_on = [
+    aws_api_gateway_integration.get_desserts_integration,
+    aws_api_gateway_integration.post_desserts_integration,
+    aws_api_gateway_integration.patch_desserts_integration,
+    aws_api_gateway_integration.delete_desserts_integration,
+  ]
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,DELETE,PATCH'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
+resource "aws_api_gateway_authorizer" "lambda_authorizer" {
+  rest_api_id                      = aws_api_gateway_rest_api.rest_api.id
+  name                             = "lambda-authorizer"
+  type                             = "REQUEST"
+  authorizer_uri                   = aws_lambda_function.desserts_api_lambda_authorizer.invoke_arn
+  authorizer_credentials           = aws_iam_role.desserts_api_role.arn
+  authorizer_result_ttl_in_seconds = 300
+  identity_source                  = "method.request.header.Authorization"
 }
