0ab2769e1348df6a6c8fb2cea130501217bf68ae
Anthony Viera <anthonyviera4@gmail.com>
2025-03-19 12:55:51 -0500

    Thanks me

--- lambdas/tests/conftest.py
+++ lambdas/tests/conftest.py
@@ -3,6 +3,17 @@ import json
 import pytest
 
 
+@pytest.fixture(autouse=True)
+def monkeypatch_env(monkeypatch):
+    monkeypatch.setenv("DYNAMODB_REGION", "us-east-1")
+    monkeypatch.setenv("DYNAMODB_ENDPOINT_URL", "http://localhost:8000")
+    monkeypatch.setenv("DYNAMODB_DESSERTS_TABLE_NAME", "desserts")
+    monkeypatch.setenv("DESSERT_IMAGES_BUCKET_NAME", "dessert-images")
+    monkeypatch.setenv("DYNAMODB_PRICES_TABLE_NAME", "prices")
+    monkeypatch.setenv("DYNAMODB_DESSERT_TYPE_COUNT_TABLE_NAME", "dessert_type_count")
+    monkeypatch.setenv("REGION", "us-east-1")
+
+
 @pytest.helpers.register
 def assert_response_equal(r1, r2):
     assert json.loads(r1.get("body")) == json.loads(r2.get("body"))
