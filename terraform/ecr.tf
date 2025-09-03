bcebd69a97a1eedac779bfbf1d7b742648ebac77
Anthony Viera <anthonyviera4@gmail.com>
2025-09-02 19:53:45 -0500

    fix

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,6 +3,6 @@ data "aws_ecr_repository" "desserts_api_lambdas" {
 }
 
 data "aws_ecr_image" "desserts_api" {
-  repository_name = data.aws_ecr_repository.desserts_api.name
+  repository_name = data.aws_ecr_repository.desserts_api_lambdas.name
   image_tag       = var.docker_image_tag # "${BRANCH_NAME}-${SHORT_SHA}" from your workflow
 }
